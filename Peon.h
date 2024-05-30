427decdd9fc2673357a6930c10b3c4a98514a8d9
sofiatoledo <80410961+sofiatoledo@users.noreply.github.com>
2024-05-30 11:39:00 +0200

    Update Peon.h

--- Peon.h
+++ Peon.h
@@ -1,1 +1,30 @@
+#pragma once
+#include "Pieza.h"
+#include "ETSIDI.h"
+#include "freeglut.h"
+#include "Tablero.h"
+using ETSIDI::SpriteSequence;
 
+
+
+
+class Peon : public Pieza
+{
+	//Accedemos a las imagenes .png para usarlas posteriormente
+	SpriteSequence imagen1{ "imagenes/PEONBLANCO.png", 1 };
+	SpriteSequence imagen2{ "imagenes/PEONNEGRO.png", 1 };
+
+public:
+
+	Peon(char _color) {
+	
+	}
+
+	//virtual~Peon() {};
+
+
+	void dibuja() override;
+
+	// PRUEBA
+	bool mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) override;
+};
