5d159fd9fe221b684c8529cc5e6014255cca4623
sofiatoledo <80410961+sofiatoledo@users.noreply.github.com>
2024-05-30 11:35:10 +0200

    Update Pieza.h

--- Pieza.h
+++ Pieza.h
@@ -1,1 +1,29 @@
+#pragma once
 
+class Tablero;
+
+enum TYPEPIEZA {
+    NONE,
+    CABALLO,
+    PEON,
+    TORRE,
+    REY,
+    REINA,
+};
+
+
+class Pieza
+{
+public:
+    TYPEPIEZA tipo;
+    int tipoFigura;
+    int x, y;
+    char color;
+    Pieza() : tipo(TYPEPIEZA::NONE), x(0), y(0) {};
+
+    virtual void dibuja() {};
+    virtual bool mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) {
+        return true;
+    };
+   
+};
