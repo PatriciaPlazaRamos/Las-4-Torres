8d0142c0a9387d63771e73bd593d108575437fa8
pablovanden <80414024+pablovanden@users.noreply.github.com>
2024-05-30 11:37:08 +0200

    Update Coordinador.h

--- Coordinador.h
+++ Coordinador.h
@@ -1,1 +1,24 @@
+#pragma once
+#include "Tablero.h"
 
+class Coordinador
+{
+public:
+	Coordinador() { estado = INICIO; };
+	virtual ~Coordinador() {};
+
+	void dibuja();
+	void tecla(unsigned char key);
+	//void onMouseClick(int button, int state, int x, int y);
+
+	friend class Tablero;
+
+
+protected:
+
+	enum Estado { INICIO, JUGAR, FIN };
+	Estado estado;
+
+	Tablero tablero;
+
+};
