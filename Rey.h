cf569a571076d79c5cd9cf487b2525839a3ff13d
PatriciaPlazaRamos <80411487+PatriciaPlazaRamos@users.noreply.github.com>
2024-05-30 11:36:20 +0200

    Update Rey.h

--- Rey.h
+++ Rey.h
@@ -1,1 +1,31 @@
+#pragma once
+#include "Pieza.h"
+#include "ETSIDI.h"
+#include "freeglut.h"
+#include "Tablero.h"
+using ETSIDI::SpriteSequence;
 
+
+
+
+class Rey : public Pieza
+{
+	//Accedemos a las imagenes .png para usarlas posteriormente
+	SpriteSequence imagen1{ "imagenes/REYBLANCO.png", 1 };
+	SpriteSequence imagen2{ "imagenes/REYNEGRO.png", 1 };
+	
+	public:
+	
+
+	Rey(char _color) {
+	
+	}
+
+	virtual~Rey() {};
+
+
+	void dibuja() override;
+
+	// PRUBEBA
+	bool mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) override;
+};
