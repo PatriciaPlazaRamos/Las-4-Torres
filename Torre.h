eda797dd53cffb760daa0cb8b9cc671c8ef1825d
PatriciaPlazaRamos <80411487+PatriciaPlazaRamos@users.noreply.github.com>
2024-05-30 11:35:40 +0200

    Update Torre.h

--- Torre.h
+++ Torre.h
@@ -1,1 +1,30 @@
+#pragma once
+#include "Pieza.h"
+#include "ETSIDI.h"
+#include "freeglut.h"
+#include "Tablero.h"
+using ETSIDI::SpriteSequence;
 
+
+
+
+class Torre : public Pieza
+{
+	//Accedemos a las imagenes .png para usarlas posteriormente
+	SpriteSequence imagen1{ "imagenes/TORREBLANCO.png", 1 };
+	SpriteSequence imagen2{ "imagenes/TORRENEGRO.png", 1 };
+
+public:
+	
+	Torre(char _color) {
+	
+	}
+
+	virtual~Torre() {};
+
+
+	void dibuja() override;
+
+	// PRUBEBA
+	bool mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) override;
+};
