b8211b0bb4e19c2f85344fcf184d4fc04df3fbbe
PatriciaPlazaRamos <80411487+PatriciaPlazaRamos@users.noreply.github.com>
2024-06-01 12:43:55 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -1,1 +1,23 @@
+#include "freeglut.h"
+#include "Reina.h"
 
+void Reina::dibuja() {
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	if (color == 'b') {
+		imagen1.draw(); //blancas
+	}
+	else if (color == 'n') {
+		imagen2.draw(); //negras
+	}
+	glPopMatrix();
+}
+
+
+bool Reina::mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) {
+
+
+
+
+}
