3846ecbd123670eef005f5b390698733e2cc76c7
PatriciaPlazaRamos <80411487+PatriciaPlazaRamos@users.noreply.github.com>
2024-06-01 12:44:28 +0200

    Update Rey.cpp

--- Rey.cpp
+++ Rey.cpp
@@ -1,1 +1,22 @@
+#include "Rey.h"
+#include "freeglut.h"
 
+
+void Rey::dibuja() {
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	if (color == 'b') {
+		imagen1.draw(); //blancas
+	}
+	else if (color == 'n') {
+		imagen2.draw(); //negras
+	}
+	glPopMatrix();
+}
+
+
+bool Rey::mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) {
+
+	
+}
