ec33bdeebf4cccd3c79043c652644244a64f7152
Jaime Torres <80216587+JA1Me23@users.noreply.github.com>
2024-06-01 11:03:50 +0200

    Update Tablero.cpp

--- Tablero.cpp
+++ Tablero.cpp
@@ -64,7 +64,37 @@ void Tablero::dibuja(int ControlTablero) {
 			}
 		}
 	}
-	
+	if (ControlTablero == 0) {//sylverman
+		margenInferior = 10;
+		margenIzquierda = 5;
+		for (int y = 0; y < 4; y++) {
+			for (int x = 0; x < 4; x++) {
+				if ((x + y) % 2 == 0) {
+					glColor3ub(26, 105, 150); // Color oscurito
+				}
+				else {
+					glColor3ub(192, 219, 235); // Color clarito
+				}
+				glBegin(GL_QUADS);
+				glVertex2i(margenIzquierda + x * (casillaTam), margenInferior + y * (casillaTam + 2));
+				glVertex2i(margenIzquierda + (x + 1) * (casillaTam), margenInferior + y * (casillaTam + 2));
+				glVertex2i(margenIzquierda + (x + 1) * (casillaTam), margenInferior + (y + 1) * (casillaTam + 2));
+				glVertex2i(margenIzquierda + x * (casillaTam), margenInferior + (y + 1) * (casillaTam + 2));
+				glEnd();
+
+				// Dibujar la pieza en la casilla
+				if (tablero[x][y] != nullptr) {
+					glPushMatrix();
+					glTranslatef(margenIzquierda + x * (casillaTam), margenInferior + y * (casillaTam + 2), 0.1);
+					tablero[x][y]->dibuja();
+					glPopMatrix();
+				}
+			}
+		}
+	}
+
+
+
 }
 
 void Tablero::inicializa(int ControlTablero) {
@@ -74,33 +104,56 @@ void Tablero::inicializa(int ControlTablero) {
 	y_ojo = 20;
 	z_ojo = 30; // habria q mirarlo bien
 
-	
-	for (int y = 0; y < 6; y++) {
-		for (int x = 0; x < 6; x++) {
-			tablero[x][y] = nullptr;
+	if (ControlTablero == 1) {
+		for (int y = 0; y < 6; y++) {
+			for (int x = 0; x < 6; x++) {
+				tablero[x][y] = nullptr;
+			}
+		}
+		turno = 1;
+
+		tablero[1][0] = new Caballo('b');
+		tablero[4][0] = new Caballo('b');
+		tablero[1][5] = new Caballo('n');
+		tablero[4][5] = new Caballo('n');
+		tablero[2][0] = new Reina('b');
+		tablero[2][5] = new Reina('n');
+		tablero[0][0] = new Torre('b');
+		tablero[5][0] = new Torre('b');
+		tablero[0][5] = new Torre('n');
+		tablero[5][5] = new Torre('n');
+		tablero[3][0] = new Rey('b');
+		tablero[3][5] = new Rey('n');
+
+
+		for (int i = 0; i < 6; i++) {
+			tablero[i][1] = new Peon('b'); //peones blancos en toda la fila 1
+			tablero[i][4] = new Peon('n'); //peones negros en toda la fila 6
 		}
 	}
-	turno = 1;
+	else {//sylverman
+		for (int y = 0; y < 4; y++) {
+			for (int x = 0; x < 4; x++) {
+				tablero[x][y] = nullptr;
+			}
+		}
 
-	tablero[1][0] = new Caballo('b');
-	tablero[4][0] = new Caballo('b');
-	tablero[1][5] = new Caballo('n');
-	tablero[4][5] = new Caballo('n');
-	tablero[2][0] = new Reina('b');
-	tablero[2][5] = new Reina('n');
-	tablero[0][0] = new Torre('b');
-	tablero[5][0] = new Torre('b');
-	tablero[0][5] = new Torre('n');
-	tablero[5][5] = new Torre('n');
-	tablero[3][0] = new Rey('b');
-	tablero[3][5] = new Rey('n');
+		turno = 1;
+		tablero[1][0] = new Reina('b');
+		tablero[1][3] = new Reina('n');
+		tablero[0][0] = new Torre('b');
+		tablero[3][0] = new Torre('b');
+		tablero[0][3] = new Torre('n');
+		tablero[3][3] = new Torre('n');
+		tablero[2][0] = new Rey('b');
+		tablero[2][3] = new Rey('n');
 
 
-	for (int i = 0; i < 6; i++) {
-		tablero[i][1] = new Peon('b'); //peones blancos en toda la fila 1
-		tablero[i][4] = new Peon('n'); //peones negros en toda la fila 6
+		for (int i = 0; i < 4; i++) {
+			tablero[i][1] = new Peon('b'); //peones blancos en toda la fila 1
+			tablero[i][2] = new Peon('n'); //peones negros en toda la fila 6
+		}
 	}
-	
 
 	
 
@@ -109,3 +162,36 @@ void Tablero::inicializa(int ControlTablero) {
 
 	//ETSIDI::play("sonidos/Empieza.wav");//tengo q implementar los sonidos
 }
+
+int xInicial = -1, yInicial = -1;
+int xFinal = -1, yFinal = -1;
+
+void Tablero::onMouseClick(int button, int state, int x, int y) {
+
+	//JAQUE MATE
+	/*if (!estaReyEnJaqueMate(*this, turno)) {
+		cout << "jaque mate" << endl;
+	}*/
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		// Calcular la posición del clic en términos de celdas del tablero
+		int casillaX = (x - 166) / 137;
+		int casillaY = (825 - y - 53) / 113;  // Convertir la coordenada y
+		std::cout << x << ":" <<casillaX<< std::endl;
+		std::cout << y << ":" << casillaY << std::endl;
+
+		
+}
+
+
+bool Tablero::estaReyEnJaque(Tablero& t, char colorpieza, bool ocupado) {
+	int yRey = -1;
+	int xRey = -1;
+
+	
+
+	// El rey no est  en jaque
+	return false;
+}
+
+
