fe279f65e7a2d823430c46722ae3af29bcf3d59c
sofiatoledo <80410961+sofiatoledo@users.noreply.github.com>
2024-06-01 15:51:52 +0200

    Update Peon.h

--- Peon.h
+++ Peon.h
@@ -17,14 +17,20 @@ class Peon : public Pieza
 public:
 
 	Peon(char _color) {
-	
+		color = _color;
+		tipo = TYPEPIEZA(PEON);
+		imagen1.setCenter(-0.7, -0.5); //blancas
+		imagen1.setSize(3, 5);
+		imagen2.setCenter(-0.7, -0.5);
+		imagen2.setSize(3, 5);
+
 	}
 
-	//virtual~Peon() {};
+	virtual~Peon() {};
 
 
 	void dibuja() override;
 
-	// PRUEBA
+	
 	bool mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) override;
 };
