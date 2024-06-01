bab6690c4b72ef105535f669feb127b54900f786
Jaime Torres <80216587+JA1Me23@users.noreply.github.com>
2024-06-01 11:00:42 +0200

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -11,6 +11,8 @@ class Tablero
 {
 private:
 	Pieza* tablero[6][6];
+	int contadorblanco;
+	int contadornegro;
 	int turno;
 	bool fin;
 
@@ -18,7 +20,7 @@ private:
 	float y_ojo;
 	float z_ojo;
 public:
-	Tablero() :turno(1) {
+	Tablero() :turno(1), contadorblanco(0), contadornegro(0), fin(0) {
 		// Inicializacion del tablero	
 		//no se si tengo q diferenciar el 4x4 y el 6x6 o puedo crear la matriz 6x6 y solo dibujar la 4x4 cuando sea necesario
 		for (int y = 0; y < 6; y++) {
@@ -31,7 +33,7 @@ public:
 	void dibuja(int);
 	void inicializa(int);
 
-	//void onMouseClick(int button, int state, int x, int y); //Obtiene la posicion del raton y mueve pieza
+	void onMouseClick(int button, int state, int x, int y); //Obtiene la posicion del raton y mueve pieza
 	static void confirmaMouse(int button, int state, int x, int y, Tablero& tablero)
 	{
 		tablero.onMouseClick(button, state, x, y);
@@ -44,5 +46,7 @@ public:
 	friend class Pieza;
 	friend class Coordinador;
 
-
+	bool estaReyEnJaque(Tablero& t, char colorRey, bool ocupado);
+	void dibujaPiezaBorrad(Tablero& t, TYPEPIEZA tipoFinal, int xFinal, int yFinal, char colorFinal);
+	
 };
