2f57c2de987f64c5fc8769956f1d19624cbb6aa2
pablovanden <80414024+pablovanden@users.noreply.github.com>
2024-06-05 18:47:29 +0200

    Update Torre.cpp

--- Torre.cpp
+++ Torre.cpp
@@ -1,1 +1,20 @@
+#include "Torre.h"
 
+
+void Torre::dibuja() {
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	if (color == 'b') {
+		imagen1.draw(); //blancas
+	}
+	else if (color == 'n') {
+		imagen2.draw(); //negras
+	}
+	glPopMatrix();
+}
+
+
+bool Torre::mover(int xInicial, int yInicial, int xFinal, int yFinal, bool ocupado, Tablero& t) {
+	
+}
